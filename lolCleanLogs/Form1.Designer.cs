4828b97643ab6e0d78f1c47fac1193fb4888223c
gafar113 <falgamerss@gmail.com>
2021-02-24 14:14:10 +0300

    Log cleaner for league of legends

---
+++ lolCleanLogs/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿
+namespace lolCleanLogs
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.cleanBtn = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // cleanBtn
+            // 
+            this.cleanBtn.Location = new System.Drawing.Point(94, 129);
+            this.cleanBtn.Name = "cleanBtn";
+            this.cleanBtn.Size = new System.Drawing.Size(140, 30);
+            this.cleanBtn.TabIndex = 0;
+            this.cleanBtn.Text = "Clean Logs";
+            this.cleanBtn.UseVisualStyleBackColor = true;
+            this.cleanBtn.Click += new System.EventHandler(this.cleanBtn_Click_1);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(195, 28);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(91, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Browse";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(164, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Select League of Legends folder:";
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(18, 77);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(280, 29);
+            this.progressBar.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(319, 180);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.cleanBtn);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "LoL Log Cleaner | Wiwy";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button cleanBtn;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar progressBar;
+    }
+}
+
---
+++ lolCleanLogs/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lolCleanLogs
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+     
+
+        static readonly string ProgramData = @"C:\ProgramData\Riot Games";
+        private static readonly string Local = @"C:\Users\faruk\AppData\Local\RiotGames";
+        private string dirName;
+        private string dirPath;
+        private bool truePath;
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            FolderBrowserDialog x = new FolderBrowserDialog();
+            x.ShowDialog();
+            dirPath = x.SelectedPath;
+            
+
+            if (x.SelectedPath.Contains("Riot Games\\League of Legends") || x.SelectedPath.Contains("Riot Games\\lol") || x.SelectedPath.Contains("League of Legends") || x.SelectedPath.Contains("lol"))
+            {
+                MessageBox.Show("True directory", "Success");
+                truePath = true;
+            }
+
+            else
+            {
+                MessageBox.Show("Wrong directory!", "Error");
+                truePath = false;
+            }
+        }
+
+        private void cleanBtn_Click_1(object sender, EventArgs e)
+        {
+            if (truePath == true)
+            {
+                try
+                {
+                    string machine = "machine.cfg";
+                    if (File.Exists(Path.Combine(ProgramData, machine)))
+                    {
+                        // If file found, delete it    
+                        File.Delete(Path.Combine(ProgramData, machine));
+                        MessageBox.Show("Machine.cfg deleted!");
+                        progressBar.Value = 25;
+                    }
+
+                    else if (!File.Exists(Path.Combine(ProgramData, machine)))
+                    {
+                        MessageBox.Show("Machine.cfg not found!", "Success");
+                        progressBar.Value = 25;
+                    }
+                }
+                catch (Exception exception)
+                {
+
+                }
+
+                try
+                {
+                    if (Directory.Exists(Local))
+                    {
+                        Directory.Delete(Local);
+                        MessageBox.Show("Appdata logs deleted!", "Success");
+                        progressBar.Value = 50;
+                    }
+                    else if (!Directory.Exists(Local))
+                    {
+                        MessageBox.Show("Appdata/RiotGames not found!", "Success");
+                        progressBar.Value = 50;
+                    }
+                }
+                catch (Exception exception)
+                {
+
+                }
+
+
+                try
+                {
+                    string config = "Config";
+                    string configdir = Path.Combine(dirPath, config);
+                    if (Directory.Exists(configdir))
+                    {
+                        Directory.Delete(configdir);
+                        MessageBox.Show("Configs deleted!", "Success");
+                        progressBar.Value = 75;
+                    }
+                    else if (!Directory.Exists(configdir))
+                    {
+                        MessageBox.Show("Configs not found!", "Success");
+                        progressBar.Value = 75;
+                    }
+
+                }
+                catch (Exception exception)
+                {
+
+                }
+
+
+                try
+                {
+                    string logs = "Logs";
+                    string logsdir = Path.Combine(dirPath, logs);
+                    if (Directory.Exists(logsdir))
+                    {
+                        Directory.Delete(logsdir);
+                        MessageBox.Show("Logs deleted!", "Success");
+                        progressBar.Value = 100;
+                    }
+                    else if (!Directory.Exists(logsdir))
+                    {
+                        MessageBox.Show("Logs not found!","Success");
+                        progressBar.Value = 100;
+                    }
+
+                }
+                catch (Exception exception)
+                {
+
+                }
+
+                MessageBox.Show("All logs has been deleted!", "Success");
+            }
+
+            if (truePath == false)
+            {
+                MessageBox.Show("Select a League of Legends directory!","Error");
+            }
+        }
+    }
+}
